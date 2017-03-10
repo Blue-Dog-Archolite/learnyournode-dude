6c95ce29dd277844c859a79d3f1b0f193c3a0d42
Blue-Dog-Archolite <blue.dog.archolite@gmail.com>
2017-03-10 16:55:36 -0700

    WIP 8

--- 8/http.js
+++ 8/http.js
@@ -1,15 +1,29 @@
 'use strict';
 
-var http = require('http');
-var url = process.argv[2];
+const BufferList = require('bl');
+const hyperquest = require('hyperquest')
+const http = require('http');
 
-http.get(url, function (resp) {
-    var body = [];
+let url = process.argv[2];
 
-    resp.setEncoding('utf8');
-    resp.on('data', console.log);
-}).on('error', function (err) {
-    console.log(err.message);
-}).on('data', function (data) {
-    consloe.log('data', data);
-});
+var result = "";
+// http.get(url, function (resp) {
+//     var body = [];
+
+//     resp.setEncoding('utf8');
+//     resp.on('data', console.log);
+// }).on('error', function (err) {
+//     console.log(err.message);
+// }).on('data', function (data) {
+//     consloe.log('data', data);
+// });
+
+
+var bl = new BufferList();
+// let url = 'https://raw.github.com/rvagg/bl/master/README.md'
+
+hyperquest(url).pipe(BufferList(function (err, data) {
+    result = result + " " + data.toString();
+}));
+
+console.log(result);
